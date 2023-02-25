710dfa60491e6bde80cb9408e40d2c7ab9955afb
junior <118004363+stratznovich@users.noreply.github.com>
2023-02-25 16:57:51 -0300

    Escreva um programa que declare um inteiro, iricialize-o com O, e incremente-o de 1000 em 1000, imprimindo seu valor na tela, até que seu valor seja 100000 mil).

--- exercicio_04.py
+++ exercicio_04.py
@@ -3,7 +3,7 @@ Escreva um programa que declare um inteiro, iricialize-o com O, e incremente-o d
 em 1000, imprimindo seu valor na tela, até que seu valor seja 100000 mil).
 '''
 
-entrada = int(input('insira ym valor: '))
+entrada = int(input('insira ym valor:'))
 limite = 100000
 while entrada < limite:
     print(entrada)
